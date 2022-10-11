c7b4a8912f00f4a5d5a3db4cc42942720a42ea01
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2022-10-11 09:40:14 +0200

    ajout de la function name_of_domain

--- utile.py
+++ utile.py
@@ -5,11 +5,11 @@ from bs4 import BeautifulSoup
 
 
 def download_book_page(url):
-    """fonction de recuparation et de parsage de la page HTML
+    """parsing of html page
         Args:
-            url : adresse URL d'un livre
+            url : url of book
         Returns:
-            : contenu de la page html en question
+            : content of page 
      """
     reponse = requests.get(url)
     soup = BeautifulSoup(reponse.content, "html.parser")
@@ -17,8 +17,28 @@ def download_book_page(url):
 
 
 def replace_special_caractere(my_str):
+    """replace special caractere
+    Args:
+        my_str (_type_): _description_
+    Returns:
+        _type_: _description_(without cspecial caractere)
+    """
     special_caractere = "}’{!@#$%^&*'()¨^\[]};,./<>?|`~-=_+:‽"
     for element in special_caractere:
         my_str = my_str.replace(element, "_").replace(
             "é", "e").replace("è", "e").replace('"', " ")
     return my_str
+
+
+def name_of_domain(url):
+    """ return the domain name
+    Args:
+        url (_type_): _description_
+    Return:
+        Url of domaine("http://books.toscrape.com/catalogue/")
+    """
+    
+    url_domain = url[:36]
+    
+    return url_domain
+
