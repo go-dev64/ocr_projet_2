4359d56661c8ce690dd98cb5976dae5df65dc52a
Guillaume <guillaume.oblin@greta-cfa-aquitaine.academy>
2022-10-11 11:24:12 +0200

    refacto category.py

--- category.py
+++ category.py
@@ -4,93 +4,102 @@ import utile
 import os
 
 
-def get_links_of_page(soupe):
-    """recupere tous les liens des livres d'une page.
-        Args:
-            Parsage d'une page html.
-        Returns:
-            une liste des liens des livres de la page html links[].
-    """
-
-    links = []
-    for i in soupe.find_all('h3'):
-        href = i.find("a")['href']
-        book_name = href.split("/").pop(-2)
-        links.append("http://books.toscrape.com/catalogue/"
-                     + book_name
-                     + "/index.html")
-    return links
-
-
-def links_all_pages(url, reponse):
-    """boucle sur toutes les pages disponibles pour la categorie
-    ou sur le site et retourne un liste des liens des livres.
+def get_url_books_from_a_single_page(soupe, url):
+    """get all books links of page.
     Args:
-        url des la categorie ou du site .
-        reponse : reponse de la requette http (requests)
+        Parsing of html page / url page
     Returns:
-        une liste contenant tous les liens des livres. books_url
+        list of links of books from a page.
     """
+    url_domaine = utile.url_domaine(url)
+    list_of_books_from_single_page = []
+    for i in soupe.find_all("h3"):
+        href = i.find("a")["href"]
+        book_name = href.split("/").pop(-2)
+        list_of_books_from_single_page.append(url_domaine
+                                              + book_name + "/index.html")
+    return list_of_books_from_single_page
 
-    links_of_books_pages = []
+
+def get_books_links_from_all_pages(url, reponse):
+    """loop through all pages for a category or site and
+    return all available books.
+    Args:
+        url of category or site .
+        response : response (requests).
+    Returns:
+        list of links of books from a category or site.
+    """
+    list_of_all_links_of_books = []
     x = 0
     while reponse.ok:
         x += 1
-        url_var = url + "page-" + str(x) + '.html'
+        url_var = url + "page-" + str(x) + ".html"
         reponse = utile.download_book_page(url_var)[1]
-        links_of_books_pages.extend(
-            get_links_of_page(
-                utile.download_book_page(url_var)[0]
+        list_of_all_links_of_books.extend(
+            get_url_books_from_a_single_page(
+                soupe=utile.download_book_page(url_var)[0], url=url
                 )
-            )
+        )
 
-    return links_of_books_pages
+    return list_of_all_links_of_books
 
 
-def get_all_links_of_all_pages(url):
-    """Fonction des recuperations de tous les liens.
+def select_case_of_application(url):
+    """selects the application case according to the url entered by the user.
     Args:
-        url de la categorie ou du site
+        url of category or site or book.
     Returns:
-        liste des url des livres.
+        list of links of books (one book,category or site).
     """
-    all_books_url = []
+    list_of_links = []
     url_modifie = url[:-10]
     reponse = utile.download_book_page(url_modifie + "page-1.html")[1]
 
     if reponse.ok:
-        """verifie si plusieurs pages sont disponible,
-        et les parcours pour retourner les liens des livres de chaque pages"""
-        all_books_url.extend(
-            links_all_pages(
+        """case of application for a request for:
+        a category of several pages or the site."""
+        list_of_links.extend(
+            get_books_links_from_all_pages(
                 url=url_modifie,
                 reponse=utile.download_book_page(url_modifie)[1]
-                )
             )
+        )
 
     elif utile.download_book_page(url)[0].find("ol", class_="row"):
-        """verifie si il y a des liens de livres sur la page
-        et retournent tous les liens de cette page. """
-        all_books_url.extend(
-            get_links_of_page(soupe=utile.download_book_page(url)[0]))
+        """case of application for a request for:
+        a single page category."""
+        list_of_links.extend(get_url_books_from_a_single_page(
+            soupe=utile.download_book_page(url)[0], url=url
+            )
+        )
 
     else:
-        """retournent le lien d'un livre"""
-        all_books_url.append(url)
-    return all_books_url
+        """case of application for a request for a book."""
+        list_of_links.append(url)
+    return list_of_links
 
 
 def category():
+    """get all categories of the site.
+
+    Returns:
+        list of categories
+    """
     soup = utile.download_book_page(
-        "http://books.toscrape.com/catalogue/category/books_1/index.html")[0]
-    categories = soup.find("ul", class_='nav').find("ul").find_all("a")
+        "http://books.toscrape.com/catalogue/category/books_1/index.html"
+    )[0]
+    categories = soup.find("ul", class_="nav").find("ul").find_all("a")
     all_category = []
     for category in categories:
         all_category.append(category.string.strip())
     return all_category
 
 
-def creating_folders_by_category():
+def creating_folders_by_categories():
+    """Creating Data directory and folder categories
+    in the current work directory
+    """
     list_of_category = category()
     if not os.path.exists("Data"):
         os.makedirs("Data")
@@ -102,4 +111,4 @@ def creating_folders_by_category():
     print("Un dossier de chaque categories a été crée dans le rep :Data")
 
 
-creating_folders_by_category()
+# creating_folders_by_categories()
